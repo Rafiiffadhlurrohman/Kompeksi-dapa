e3b35504c4a22e33a3e33c760c8d9df0bf392542
RAFIIF <rafiiffadlurrohman99@gmail.com>
2024-12-08 15:12:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ def subtract_stock(data, name, size, quantity):
 
 # Streamlit app
 def main():
-    st.title(":rainbow[KONVEKSI DAVA]")
+    st.title(":blue[KONVEKSI DAVA]")
 
     filename = 'data_stok_kompeksi.txt'
     data = load_data(filename)
